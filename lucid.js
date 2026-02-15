74fae674b204e4aacb00eb99e2a29648f4411ae2
Mokut001 <mokutoton01@gmail.com>
2026-02-16 00:58:59 +0100

    Add files via upload

---
+++ lucid.js
@@ -0,0 +1,16 @@
+import { Lucid, Blockfrost } from "lucid-cardano";
+
+export async function connectWallet() {
+  const lucid = await Lucid.new(
+    new Blockfrost(
+      "https://cardano-preprod.blockfrost.io/api/v0",
+      process.env.NEXT_PUBLIC_BLOCKFROST_KEY
+    ),
+    "Preprod"
+  );
+
+  const api = await window.cardano.nami.enable();
+  lucid.selectWallet(api);
+
+  return lucid;
+}
