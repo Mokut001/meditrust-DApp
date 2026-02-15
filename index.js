2da4a42c8d0a46b7498951ef8b4f8886a94bcab3
Mokut001 <mokutoton01@gmail.com>
2026-02-16 00:58:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding:40}}>
+      <h1>MediTrust (Preprod)</h1>
+      <p>Decentralized Hospital Authorization System on Cardano</p>
+      <a href="/dashboard">Go to Dashboard</a>
+    </div>
+  );
+}
