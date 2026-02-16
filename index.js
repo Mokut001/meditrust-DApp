c324d21789c59bad25f4041e93c2783950257095
Mokut001 <mokutoton01@gmail.com>
2026-02-16 02:18:30 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding:40}}>
-      <h1>MediTrust (Preprod)</h1>
-      <p>Decentralized Hospital Authorization System on Cardano</p>
-      <a href="/dashboard">Go to Dashboard</a>
-    </div>
-  );
-}
